8755148cbc18d7799b6ef31159e5e0800ddfb08b
Guy Martin <gmsoft@tuxicoman.be>
2012-08-07 15:53:42 +0200

    Fetch serials on startup. Add support for logs.

--- pomng/pom.py
+++ pomng/pom.py
@@ -7,17 +7,27 @@ from . import registry
 class pom:
 
 	proxy = None
-	serials = { 'main' : 0, 'registry' : 0 }
+	logLevel = 3
 
 	def __init__(self, url):
 		self.url = url
 		self.proxy = xmlrpc.client.ServerProxy(url)
 		self.registry = registry.registry(self.proxy)
+		self.serials = self.proxy.core.serialPoll(0)
 		_thread.start_new_thread(self.pollSerial, (xmlrpc.client.ServerProxy(url), ))
 
 	def getVersion(self):
 		return self.proxy.core.getVersion()
 
+	def setLoggingLevel(self, level):
+		self.logLevel = level
+
+	def updateLogs(self, proxy, logId):
+		logs = proxy.core.getLog(logId)
+		for log in logs:
+			if log['level'] <= self.logLevel:
+				print(log['data'])
+			
 	def pollSerial(self, pollProxy):
 		while True:
 			try:
@@ -25,6 +35,10 @@ class pom:
 			except Exception as e:
 				print("Error while polling pom-ng :", e)
 				os._exit(1)
+
 			if serials['registry'] != self.serials['registry']:
 				self.registry.update(pollProxy)
+			if serials['log'] != self.serials['log']:
+				self.updateLogs(pollProxy, self.serials['log'])
+			
 			self.serials = serials
